e84037f7dea565cd3362f8903963a8d29025d74d
Tomas Peters <tdpeters6794@gmail.com>
2024-10-10 15:17:32 -0400

    more clean json reading

--- src/components/backgrounds/BackgroundSummary.tsx
+++ src/components/backgrounds/BackgroundSummary.tsx
@@ -3,22 +3,33 @@ import { Background } from '../../interfaces'
 export default function BackgroundSummary(props: any) {
 	const background: Background = props.background
 
-	const equipment = background.startingEquipment[0]._ // Access the first item in the array
+	// const equipment = background.startingEquipment[0]._ // Access the first item in the array
 
-	const formattedEquipment = equipment.map(item => {
-		if (typeof item === 'string') {
-			return item.split('|')[0] // Split to get only the name part
-		} else if (item.item) {
-			return item.displayName || item.item.split('|')[0] // Use displayName or fallback to item name
-		} else if (item.special) {
-			return item.quantity
-				? `${item.quantity} ${item.special}`
-				: item.special // Handle special cases
-		}
-		return ''
-	})
+	// const formattedEquipment = equipment.map(item => {
+	// 	if (typeof item === 'string') {
+	// 		return item.split('|')[0] // Split to get only the name part
+	// 	} else if (item.item) {
+	// 		return item.displayName || item.item.split('|')[0] // Use displayName or fallback to item name
+	// 	} else if (item.special) {
+	// 		return item.quantity
+	// 			? `${item.quantity} ${item.special}`
+	// 			: item.special // Handle special cases
+	// 	}
+	// 	return ''
+	// })
 
-	const lastItem = formattedEquipment.pop() // Remove the last item for formatting
+	// const lastItem = formattedEquipment.pop() // Remove the last item for formatting
+
+	const cleanText = string => {
+		const cleanedString = string
+			.replace(/@item/g, '')
+			.replace(/@skill/g, '')
+			.replace(/\|/g, '')
+			.replace(/\{|\}/g, '')
+			.replace(/phb/g, '')
+			.trim()
+		return cleanedString
+	}
 
 	return (
 		<div className="background-summary" style={{ backgroundColor: 'red' }}>
@@ -26,7 +37,7 @@ export default function BackgroundSummary(props: any) {
 				<span className="detail-key">Source:</span>
 				{background.source}
 			</div>
-			<div className="skill-proficiencies">
+			{/* <div className="skill-proficiencies">
 				<span className="detail-key">Skill Proficiences:</span>
 				<div>
 					{Object.keys(background.skillProficiencies[0]).map(key => (
@@ -44,8 +55,8 @@ export default function BackgroundSummary(props: any) {
 						standard language
 					</div>
 				</div>
-			)}
-			{background.toolProficiencies && (
+			)} */}
+			{/* {background.toolProficiencies && (
 				<div className="language-proficiencies">
 					<span className="detail-key">Tool Proficiences: </span>
 					<div style={{ textTransform: 'capitalize' }}>
@@ -56,30 +67,49 @@ export default function BackgroundSummary(props: any) {
 						)}
 					</div>
 				</div>
-			)}
-			{background.startingEquipment && (
+			)} */}
+			{/* {background.startingEquipment && (
 				<div className="starting-equipment">
 					<span className="detail-key">Starting Equipment:</span>
 					{formattedEquipment.join(', ')}
 					{lastItem && `, and ${lastItem}`}
 				</div>
-			)}
+			)} */}
 
 			<div className="entries">
-				<span className="detail-key">Entries:</span>
 				<ul className="entries-list">
-					{Array.isArray(background.entries)
+					{/* {Array.isArray(background.entries)
 						? background.entries.map(entry => (
+                            <>
+                            {entry.type === "list" && 
+                                <li>
+                                <span className='entry-name'>{entry.item}</span>
+                                </li>}
 								<li>
 									<span className="entry-name">
 										{entry.name}
 									</span>
 									{JSON.stringify(entry.entries)}
-								</li>
+								</li></>
 							))
-						: JSON.stringify(background.entries)}
+						: JSON.stringify(background.entries)} */}
+					{background.entries[0].items.map((item, index) => (
+						<li key={index}>
+							<span className="detail-key">
+								{cleanText(item.name)}
+							</span>{' '}
+							{cleanText(item.entry)}
+						</li>
+					))}
+					{background.entries[1].data && (
+						<li>
+							<span className="detail-key">
+								{background.entries[1].name}
+							</span>{' '}
+							{background.entries[1].entries}
+						</li>
+					)}
 				</ul>
-				{JSON.stringify(background.startingEquipment)}
 			</div>
 		</div>
 	)
